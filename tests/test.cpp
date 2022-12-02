564a8e59860b9eaf3221f37e779dfdb46ce08627
Jeremy Bonnefoi <jeremybonnefoi@yncrea.isen.fr>
2022-12-02 09:34:24 +0100

    init testing

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,19 +1,5 @@
 #include <catch2/catch_test_macros.hpp>
-#include "SDL.h"
-#include "../Scene.hpp"
-#include "../Rectangle.hpp"
-
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
 
 TEST_CASE( "Add a rect in scene") {
-    Sq::Scene s;
-    s.set_debug();
-    s.set_origin(Vector{ 100,100 });
-
-    Sq::Rectangle player(&s, 500, 0, 30, 30, Color{ 255,0,255 });
-    s.add_item(&player);
-    int verif = 1;
-    REQUIRE( verif == 1);
+    REQUIRE( 2 == 1);
 }
