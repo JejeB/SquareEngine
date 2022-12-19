6bc75cfe13fcecb952c89b9df63273495b7ca752
Jeremy Bonnefoi <jeremybonnefoi@yncrea.isen.fr>
2022-12-19 10:40:24 +0100

    add recurtion to collision resolution

--- DynamicRectangle.cpp
+++ DynamicRectangle.cpp
@@ -1,4 +1,6 @@
 #include "DynamicRectangle.hpp"
 
+#include "SDL.h"
+
 Sq::DynamicRectangle::DynamicRectangle(float x, float y, float width, float height, const GraphicObject* parent) :Sq::Rectangle(x, y, width, height, parent), _velocity(Vector{0,0}) 
 {}
